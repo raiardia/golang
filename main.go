6f26077eb78a13773db1c75ef5b874f474280099
raiardia <ardiaanggreni@gmail.com>
2025-03-21 19:00:31 +0800

    3. HTTP Server Sederhana dengan net/http

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main // package declaration
-import "fmt" // import statement
-func main() { // main function declaration
-	fmt.Println("Hello, World!") // function call
+import (
+	"fmt"      // import statement
+	"net/http" // import statement
+)
+
+func handler(w http.ResponseWriter, r *http.Request) { // function declaration	
+	fmt.Fprintf(w, "Hello, World!") // function call
+}
+func main() { 
+	http.HandleFunc("/", handler) // function call
+	http.ListenAndServe(":3000", nil) // function call
 }
